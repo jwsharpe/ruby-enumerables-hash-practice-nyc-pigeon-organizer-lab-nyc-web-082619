8300213632f0bcd6914a47db6af24e81525d8385
James Sharpe <jameswrightsharpe@gmail.com>
2019-08-04 23:24:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,44 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  name_hash = Hash.new
+  attribute_hash = Hash.new
+  data.each do |attribute, states|
+    state_array = []
+    attribute_hash[attribute] = state_array
+    states.each do |state, names|
+      for name in names do
+        state_array.push(state)
+        name_hash[name] = attribute_hash
+      end
+    end
+  end
+  pp name_hash
 end
+
+'
+pigeon_list = {
+  "Theo" => {
+    :color => ["purple", "grey"],
+    :gender => ["male"],
+    :lives => ["Subway"]
+  },
+  '
+  
+pigeon_data = {
+  :color => {
+    :purple => ["Theo", "Peter Jr.", "Lucky"],
+    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+    :brown => ["Queenie", "Alex"]
+  },
+  :gender => {
+    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+    :female => ["Queenie", "Ms. K"]
+  },
+  :lives => {
+    "Subway" => ["Theo", "Queenie"],
+    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+    "Library" => ["Peter Jr."],
+    "City Hall" => ["Andrew"]
+  }
+}
+nyc_pigeon_organizer(pigeon_data)
